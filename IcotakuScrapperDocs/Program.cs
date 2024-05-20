241dd8d6481d37fb36a11f4a05da305eacba054b
ntxsay <ntxsay@hotmail.fr>
2024-05-20 17:23:04 -0400

    MAJ

--- IcotakuScrapperDocs/Program.cs
+++ IcotakuScrapperDocs/Program.cs
@@ -14,18 +14,6 @@ namespace IcotakuScrapperDocs
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             
             await builder.Build().RunAsync();
-
-            //Initialise la connexion à la base de données SQLite
-            IcotakuScrapper.DatabaseHandler.InitializeConnexion(IcotakuScrapper.DatabaseHandler.TestDbFilePath);
-
-            //Crée les tables de la base de données SQLite
-            await IcotakuScrapper.DatabaseHandler.CreateTablesAsync();
-
-            //Interdit l'accès au contenu adulte au sein de l'application
-            IcotakuScrapper.Options.IsAccessingToAdultContent = false;
-
-            //Autorise l'accès au contenu explicite au sein de l'application
-            IcotakuScrapper.Options.IsAccessingToExplicitContent = true;
         }
     }
 }
